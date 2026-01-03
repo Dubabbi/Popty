921634f323734dcc1a9e8ebe819e680d972db5ac
Dubabbi <thdms3160@naver.com>
2026-01-03 20:24:05 +0900

    feat: 페이지 이동 시 상단으로 올라가도록 수정

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "@/styles/global.css";
 import "@/styles/scrollToTop.css";
+import ScrollToTop from "@/components/scroll-to-top/ScrollToTop";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <NavermapsProvider ncpKeyId={import.meta.env.VITE_NAVER_MAPS_KEY}>
+        <ScrollToTop />
         <App />
       </NavermapsProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
